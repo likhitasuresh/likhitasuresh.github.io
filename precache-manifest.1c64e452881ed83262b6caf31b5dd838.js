d808a709d50a7783ceef36abb9f87bcb908681e9
Likhita Suresh <57075028+likhitasuresh@users.noreply.github.com>
2021-01-19 10:32:32 -0800

    initial commit

---
+++ precache-manifest.1c64e452881ed83262b6caf31b5dd838.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9c790e7dde83d679aef6223094ff36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4cd12ecde3a355bf474",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "fef22f0f6622a364277f",
+    "url": "/static/css/main.e4046b1d.chunk.css"
+  },
+  {
+    "revision": "f4cd12ecde3a355bf474",
+    "url": "/static/js/2.46e21464.chunk.js"
+  },
+  {
+    "revision": "5ab1baa52e351a54fb487f180027ee45",
+    "url": "/static/js/2.46e21464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef22f0f6622a364277f",
+    "url": "/static/js/main.a9d81eae.chunk.js"
+  },
+  {
+    "revision": "f6fac08a1749a9dbaa22",
+    "url": "/static/js/runtime-main.a106779c.js"
+  },
+  {
+    "revision": "ce56b96f90f5f3110be0a457cc7fb430",
+    "url": "/static/media/Logo.ce56b96f.svg"
+  },
+  {
+    "revision": "f023699bbcb7f20524ae62e697690421",
+    "url": "/static/media/android_icon.f023699b.svg"
+  },
+  {
+    "revision": "7ad7c11c3c9c9de3c81e090085c14e71",
+    "url": "/static/media/bgabout.7ad7c11c.gif"
+  },
+  {
+    "revision": "789ede2c519fbcddab903b1bf18ca37f",
+    "url": "/static/media/copyright.789ede2c.svg"
+  },
+  {
+    "revision": "ee9d964e17972a2755029fa6f9c76ec1",
+    "url": "/static/media/database_icon.ee9d964e.svg"
+  },
+  {
+    "revision": "ce241d1493ef3fab244e7986bb559a9e",
+    "url": "/static/media/dev_icon.ce241d14.svg"
+  },
+  {
+    "revision": "71123323a445526d1aa45623bbb54e59",
+    "url": "/static/media/email.71123323.svg"
+  },
+  {
+    "revision": "2be8f7bbf7f17c8c1b61880c0e72d685",
+    "url": "/static/media/flag.2be8f7bb.svg"
+  },
+  {
+    "revision": "803c466bb5e3ff7bba918bfd9bb192ee",
+    "url": "/static/media/frontida.803c466b.png"
+  },
+  {
+    "revision": "9eb3e87ac353e70782f8f82f8c8a6bcf",
+    "url": "/static/media/giithub.9eb3e87a.svg"
+  },
+  {
+    "revision": "96d1b810f5eb3faa97dc66f1af4a84e3",
+    "url": "/static/media/html_icon.96d1b810.svg"
+  },
+  {
+    "revision": "84c69532bac5e1166593c9c9fc162142",
+    "url": "/static/media/linkedin.84c69532.svg"
+  },
+  {
+    "revision": "412bc01ae1023d7b50c9f43b50fb17f8",
+    "url": "/static/media/pencil_icon.412bc01a.svg"
+  },
+  {
+    "revision": "428f680622eb2bc165400dd225e6a555",
+    "url": "/static/media/pic.428f6806.png"
+  },
+  {
+    "revision": "b0c06641841acae912437894c6ee6a41",
+    "url": "/static/media/tools_icon.b0c06641.svg"
+  },
+  {
+    "revision": "1a4f88e3956a2f4003ce229f1b5ce9fb",
+    "url": "/static/media/vsco.1a4f88e3.svg"
+  }
+]);
